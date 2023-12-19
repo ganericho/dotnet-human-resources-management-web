32913dfee16583d7661a1839e9ec756203326f45
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-12-19 14:27:03 +0800

    api : add default value to exception variable in RepositoryHandler

--- api/Utilities/Handlers/RepositoryHandler.cs
+++ api/Utilities/Handlers/RepositoryHandler.cs
@@ -3,6 +3,6 @@
 public class RepositoryHandler<T>
 {
     public bool IsFailed { get; set; } = false;
-    public string? Exception { get; set; }
+    public string? Exception { get; set; } = MessageCollection.NoException;
     public T? Result { get; set; }
 }
