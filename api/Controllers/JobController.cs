4a1ea8e3ad468e1f9e0da1dca307291e200e2893
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-12-19 20:43:12 +0800

    api : completed get all, get by guid, update and delete for job endpoint

--- api/Controllers/JobController.cs
+++ api/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 ﻿using api.Contracts;
 using api.Dtos.JobDto;
+using api.Dtos.JobDtos;
 using api.Utilities;
 using api.Utilities.Handlers;
 using Microsoft.AspNetCore.Mvc;
@@ -32,22 +33,19 @@ public class JobController : ControllerBase
         {
             var getJobs = jobRepository.GetAll();
 
-            if (getJobs.IsFailed)
+            if (getJobs.IsFailedOrEmpty)
             {
-                throw new Exception(getJobs.Exception);
+                throw getJobs.Exception;
             }
 
             // Convert repository result to dto format
-            IEnumerable<JobDto> jobDto = getJobs.Result.Select(job => (JobDto) job);
+            IEnumerable<JobDto> jobDto = getJobs.Data.Select(job => (JobDto) job);
 
             return Ok(ResponseOkHandler.Success(jobDto));
         }
         catch(Exception ex)
         {
-            if(ex.Message.Contains(MessageCollection.DataNotFound))
-            {
-                return NotFound(ResponseErrorHandler.NotFound(ex.Message));
-            }
+
 
             return StatusCode(StatusCodes.Status500InternalServerError, ResponseErrorHandler.InternalServerError(ex.Message));
         }
@@ -61,23 +59,192 @@ public class JobController : ControllerBase
             // Get job data with specified id.
             var getJob = jobRepository.GetByGuid(guid);
 
-            if (getJob.IsFailed)
+            if(getJob.Status == RepositoryStatus.ERROR)
             {
-                throw new Exception(getJob.Exception);
+                throw getJob.Exception;
             }
+
+            if(getJob.Status == RepositoryStatus.NOT_FOUND)
+            {
+                return NotFound(ResponseErrorHandler.NotFound("Job with specified ID not found."));
+            }
+
+            // Create Job Detail Dto
+            JobDetailDto jobDetailDto = (JobDetailDto) getJob.Data;
 
             // Get a list of employees with related jobs.
             var getEmployees = employeeRepository.GetByJob(guid);
 
-            if (getEmployees.IsFailed) { }
+            if(getEmployees.Status == RepositoryStatus.ERROR)
+            {
+                throw getEmployees.Exception;
+            }
 
-            return Ok(ResponseOkHandler.Success(getJob.Result));
+            if(getEmployees.Status == RepositoryStatus.NOT_FOUND)
+            {
+                jobDetailDto.Employees = Enumerable.Empty<JobEmployeeDto>();
+                
+                return Ok(ResponseOkHandler.Success(jobDetailDto));
+            }
+
+            // Get Accounts and Departments
+            var getAccounts = accountRepository.GetAll();
+
+            if (getAccounts.Status != RepositoryStatus.SUCCESS)
+            {
+                throw getAccounts.Exception;
+            }
+
+            var getDepartments = departmentRepository.GetAll();
+
+            if (getDepartments.Status != RepositoryStatus.SUCCESS)
+            {
+                throw getDepartments.Exception;
+            }
+
+            // Create Data For Job Employees
+            jobDetailDto.Employees = from employee in getEmployees.Data
+                                     join account in getAccounts.Data on employee.Guid equals account.Guid
+                                     join department in getDepartments.Data on employee.DepartmentGuid equals department.Guid
+                                     select new JobEmployeeDto
+                                     {
+                                         Guid = employee.Guid,
+                                         FirstName = employee.FirstName,
+                                         LastName = employee.LastName,
+                                         Gender = employee.Gender,
+                                         BirthDate = employee.BirthDate,
+                                         HiringDate = employee.HiringDate,
+                                         PhoneNumber = employee.PhoneNumber,
+                                         Email = account.Email,
+                                         Department = department.Name
+                                     };
+
+
+            // Success Response with employees 
+            return Ok(ResponseOkHandler.Success(jobDetailDto));
         }
         catch(Exception ex)
         {
-            if (ex.Message.Contains(MessageCollection.DataNotFound))
+            return StatusCode(StatusCodes.Status500InternalServerError, ResponseErrorHandler.InternalServerError(ex.Message));
+        }
+    }
+
+    [HttpDelete("{guid}")]
+    public IActionResult Delete(Guid guid)
+    {
+        try
+        {
+            // Check data availability.
+            var getJob = jobRepository.GetByGuid(guid);
+
+            if(getJob.Status == RepositoryStatus.ERROR)
             {
-                return NotFound(ResponseErrorHandler.NotFound(MessageCollection.JobNotFound));
+                throw getJob.Exception;
+            }
+
+            if(getJob.Status == RepositoryStatus.NOT_FOUND)
+            {
+                return NotFound(ResponseErrorHandler.NotFound("Job with specified ID not found."));
+            }
+
+            // Delete Job
+            var deleteJob = jobRepository.Delete(getJob.Data);
+
+            if(deleteJob.Status == RepositoryStatus.ERROR)
+            {
+                throw deleteJob.Exception;
+            }
+
+            return Ok(ResponseOkHandler.DeleteSuccess());
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ResponseErrorHandler.InternalServerError(ex.Message));
+        }
+    }
+
+    [HttpPost]
+    public IActionResult Create(CreateJobDto createData)
+    {
+        try
+        {
+            // Create job data.
+            var createJob = jobRepository.Create(createData);
+
+            if(createJob.Status == RepositoryStatus.ERROR)
+            {
+                throw createJob.Exception;
+            }
+
+            // Return success response
+            return Ok(ResponseOkHandler.CreateSuccess());
+        }
+        catch(Exception ex)
+        {
+            string? innerException = ex.InnerException.Message ;
+
+            if (innerException.Contains("code"))
+            {
+                return Conflict();
+            }
+
+            if (innerException.Contains("name"))
+            {
+                return Conflict();
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError, ResponseErrorHandler.InternalServerError(ex.Message));
+        }
+    }
+
+    [HttpPut()]
+    public IActionResult Update(JobDto jobDto)
+    {
+        try
+        {
+            // Check data availability.
+            var getJob = jobRepository.GetByGuid(jobDto.Guid);
+
+            if(getJob.Status == RepositoryStatus.ERROR)
+            {
+                throw getJob.Exception;
+            }
+
+            if(getJob.Status == RepositoryStatus.NOT_FOUND)
+            {
+                return NotFound(ResponseErrorHandler.NotFound("Job with specified ID not found."));
+            }
+
+            // Update job
+            var job = getJob.Data;
+            job.ModifiedDate = DateTime.Now;
+            job.Code = jobDto.Code;
+            job.Name = jobDto.Name;
+            job.MinSalary = jobDto.MinSalary;
+            job.MaxSalary = jobDto.MaxSalary;
+
+            var updateJob = jobRepository.Update(job);
+
+            if(updateJob.Status != RepositoryStatus.SUCCESS)
+            {
+                throw updateJob.Exception;
+            }
+
+            // Success Response
+            return Ok(ResponseOkHandler.UpdateSuccess());
+        }
+        catch(Exception ex)
+        {
+            string? innerException = ex.InnerException.Message;
+
+            if (innerException.Contains("code"))
+            {
+                return Conflict();
+            }
+
+            if (innerException.Contains("name"))
+            {
+                return Conflict();
             }
 
             return StatusCode(StatusCodes.Status500InternalServerError, ResponseErrorHandler.InternalServerError(ex.Message));
