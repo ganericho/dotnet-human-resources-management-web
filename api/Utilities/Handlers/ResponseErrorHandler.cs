d037bf1bde5c0afef453c889b47df1758052a8e8
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-12-19 14:09:59 +0800

    api : add controller response format

---
+++ api/Utilities/Handlers/ResponseErrorHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Security;
+
+namespace api.Utilities.Handlers;
+
+public class ResponseErrorHandler
+{
+    public int Code { get; set; }
+    public string Status { get; set; } 
+    public string Message { get; set; }
+
+    public static ResponseErrorHandler InternalServerError(string message)
+    {
+        return new ResponseErrorHandler
+        {
+            Code = StatusCodes.Status500InternalServerError,
+            Status = HttpStatusCode.InternalServerError.ToString(),
+            Message = message
+        };
+    }
+
+    public static ResponseErrorHandler NotFound(string message)
+    {
+        return new ResponseErrorHandler
+        {
+            Code = StatusCodes.Status404NotFound,
+            Status = HttpStatusCode.NotFound.ToString(),
+            Message = message
+        };
+    }
+}
---
+++ api/Utilities/Handlers/ResponseOkHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+namespace api.Utilities.Handlers;
+
+public class ResponseOkHandler
+{
+    public int Code { get; set; } = StatusCodes.Status200OK;
+    public string Status { get; set; } = HttpStatusCode.OK.ToString();
+    public string Message { get; set; } = MessageCollection.SuccessRetrieveData;
+    public object? Data { get; set; } = MessageCollection.Empty;
+
+    public ResponseOkHandler() { }
+
+    public static ResponseOkHandler Success(object data)
+    {
+        return new ResponseOkHandler
+        {
+            Data = data
+        };
+    }
+
+    public static ResponseOkHandler Success()
+    {
+        return new ResponseOkHandler();
+    }
+}
